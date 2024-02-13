dac1b5e2e88e4b68025d3fabe186e6226c1fa3d3
carsonadd05 <157738396+carsonadd05@users.noreply.github.com>
2024-02-13 15:57:15 -0600

    Update cat.py

--- cat.py
+++ cat.py
@@ -9,11 +9,11 @@ class Cat():
 #Create objects here
 #These should NOT be indented inside the class
 
-Stella = Cat()
-Stella.name = "Stella" 
-Stella.age = 7 
+stella = Cat()
+stella.name = "Stella" 
+stella.age = 7 
 
-Garfield = Cat()
-Garfield.name = "Garfield"
-Garfield.age = 50
+garfield = Cat()
+garfield.name = "Garfield"
+garfield.age = 50
 #garfield.speak()
