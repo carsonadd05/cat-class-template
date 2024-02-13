cdc26803f52629f57c9a74f0f0329124df6e9281
carsonadd05 <157738396+carsonadd05@users.noreply.github.com>
2024-02-13 15:36:10 -0600

    cat.py

--- cat.py
+++ cat.py
@@ -1,6 +1,31 @@
 #Revmoe pass and complete the cat class
 class Cat():
-    pass
+    def __init__(self):
+        self.name = "unkown"
+        self.age = 0 
+    
+    def speak(self):
+        return "Meow"
+
+
+stella = Cat
+stella.name = "stella" 
+stella.age = 7 
+word2 = stella.speak 
+print(word2)
+
+garfield = Cat
+garfield.name = "garfield"
+garfield.age = 50
+word = garfield.speak()
+print(word)
+
+
+
+
+
+
+
 
 
 #Create objects here
